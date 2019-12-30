3ba33ad722136bd411191c430a4724606a6fe9fd
Samseppiol <j_hage@live.com>
2019-12-30 13:07:02 +1100

    Update component to remove deprecated cfndsl syntax

--- sqs.cfndsl.rb
+++ sqs.cfndsl.rb
@@ -4,7 +4,10 @@ CloudFormation do
   tags << { Key: 'Environment', Value: Ref(:EnvironmentName) }
   tags << { Key: 'EnvironmentType', Value: Ref(:EnvironmentType) }
 
+  extra_tags = external_parameters.fetch(:extra_tags, [])
   extra_tags.each { |key,value| tags << { Key: key, Value: value } } if defined? extra_tags
+
+  queues = external_parameters.fetch(:queues, [])
   filter = /[^0-9a-z ]/i
   queues.each do |queue|
       logical_id = queue['name'].gsub(filter, '')
@@ -19,18 +22,18 @@ CloudFormation do
           KmsDataKeyReusePeriodSeconds queue['kms_data_key_reuse_seconds'] if queue.has_key?('kms_data_key_reuse_seconds')
 
           if queue.has_key?('redrive_policy')
-          RedrivePolicy ({
-              deadLetterTargetArn: FnGetAtt(queue['redrive_policy']['queue'].gsub(filter, ''), 'Arn'),
-              maxReceiveCount: queue['redrive_policy']['count']
-          })
+            RedrivePolicy ({
+                deadLetterTargetArn: FnGetAtt(queue['redrive_policy']['queue'].gsub(filter, ''), 'Arn'),
+                maxReceiveCount: queue['redrive_policy']['count']
+            })
           end
 
           if (queue.has_key?('fifo_queue')) && (queue['fifo_queue'])
-          FifoQueue true
-          ContentBasedDeduplication queue['content_based_deduplication'] if queue.has_key?('content_based_deduplication')
+            FifoQueue true
+            ContentBasedDeduplication queue['content_based_deduplication'] if queue.has_key?('content_based_deduplication')
           end
 
-          Property('Tags', tags + [{ Key: 'Name', Value: FnJoin('-', [ Ref(:EnvironmentName), queue['name'] ]) }])
+          Tags tags + [{ Key: 'Name', Value: FnJoin('-', [ Ref(:EnvironmentName), queue['name'] ]) }]
       end
 
       Output("#{logical_id}QueueUrl") {
