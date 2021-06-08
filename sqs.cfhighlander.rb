369e8933ea424e68d9e73cd066fd830fdcc690bf
Guslington <guslington@gmail.com>
2021-06-08 14:43:00 +1000

    cleanup

--- sqs.cfhighlander.rb
+++ sqs.cfhighlander.rb
@@ -3,8 +3,6 @@ CfhighlanderTemplate do
   ComponentVersion component_version
   Description "#{component_name} - #{component_version}"
 
-  DependsOn 'lib-iam@0.1.0'
-
   Parameters do
     ComponentParam 'EnvironmentName', 'dev', isGlobal: true
     ComponentParam 'EnvironmentType', 'development', isGlobal: true, allowedValues: ['development', 'production']
